95d03674a5b645c992ac2bf6a1d568fadee4c96e
Ihsan Younes <ihsan.younes@yapily.com>
2020-10-07 20:26:47 +0100

    Controller query parameters

--- src/main/java/com/polls/controllers/PollsController.java
+++ src/main/java/com/polls/controllers/PollsController.java
@@ -1,9 +1,11 @@
 package com.polls.controllers;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,9 @@ public class PollsController {
 
     @RequestMapping("/polls")
     @ResponseStatus(HttpStatus.OK)
-    public Collection<String> getPolls() {
+    public Collection<String> getPolls(@RequestParam(value = "userId", required = false) final Long userId,
+                                       @RequestParam(value = "title", required = false) final String title,
+                                       @RequestParam(value = "date", required = false) final Date date) {
         return null;
     }
 }
--- src/test/java/com/polls/controllers/PollsControllerTest.java
+++ src/test/java/com/polls/controllers/PollsControllerTest.java
@@ -1,8 +1,13 @@
 package com.polls.controllers;
 
+import java.time.Instant;
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest
@@ -12,7 +17,17 @@ class PollsControllerTest {
     private PollsController pollsController;
 
     @Test
-    void getPolls() {
-        pollsController.getPolls();
+    void getPollsByUserId() {
+        assertNotNull(pollsController.getPolls(1L, null, null));
+    }
+
+    @Test
+    void getPollsByTitle() {
+        assertNotNull(pollsController.getPolls(null, "title", null));
+    }
+
+    @Test
+    void getPollsAfterDate() {
+        assertNotNull(pollsController.getPolls(null, null, Date.from(Instant.now())));
     }
 }
